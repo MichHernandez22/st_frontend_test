6044f5edbb76f63c9e6a2a33a9f8afb5a3a80f04
Ana Michelle Lopez Hernandez <ing.amichellelopezhdz@gmail.com>
2025-11-10 22:57:22 -0600

    Se agrega navegacion inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home/Home';
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Home: React.FC = () => {
+    return (
+        <div>
+            <header>
+                <h1>Gestion de Usuarios</h1>
+            </header>   
+        </div>
+    );
+}
